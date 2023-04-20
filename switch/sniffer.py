16ac2be5586c1a7a42733a5775e7af0e111b2a64
Real-4339 <qvadym@stuba.sk>
2023-04-20 21:41:35 +0200

    working on switch, i may change sniffer in the future

--- switch/sniffer.py
+++ switch/sniffer.py
@@ -1,8 +1,36 @@
+from typing import Callable
 from scapy.all import AsyncSniffer, Packet
+from exceptions import SnifferIsActive
 
 
 class Sniffer:
-    def __init__(self, inerface: str):
+    def __init__(self, inerface: str, handler: Callable[[str, Packet], None] = None) -> None:
         self.__interface = inerface
         self.__running = False
-        self.__sniffer = AsyncSniffer()
+        self.__sniffer = AsyncSniffer(iface=inerface, prn=self.__handlePacket, store=False)
+        self.__packet_handler = handler
+
+    @property
+    def running(self) -> bool:
+        return self.__running
+    
+    @property
+    def interface(self) -> str:
+        return self.__interface
+    
+    def __handlePacket(self, packet: Packet) -> None:
+        if self.__packet_handler is not None:
+            self.__packet_handler(self.__interface, packet)
+
+    def start(self) -> None:
+        if self.__running:
+            raise SnifferIsActive
+        self.logger.info('Starting sniffer on interface: %s', self.__interface)
+        self.__running = True
+        self.__sniffer.start()
+
+    def stop(self) -> None:
+        self.__running = False
+        self.__sniffer.stop()
+        self.logger.info('Stopping sniffer on interface: %s', self.__interface)
+        self.__packet_handler = None
--- switch/switch.py
+++ switch/switch.py
@@ -1,5 +1,8 @@
-from switch.inter_mg import InterfaceManager
-from exceptions import InterfaceDoesNotExist, InvalidState, SwitchIsActive, SwitchIsNotActive
+import logging
+
+from inter_mg import InterfaceManager
+from mac import MacTable
+from exceptions import SwitchIsActive
 
 class Switch:
     __version__ = '3.0.1'
@@ -7,3 +10,36 @@ class Switch:
     def __init__(self) -> None:
         self.__running = False
         self.__interface_manager = InterfaceManager()
+        self.__mac_table = MacTable()
+        self.logger = self.__setLogger('DEBUG')
+
+    @property
+    def running(self) -> bool:
+        return self.__running
+    
+    @property
+    def logger(self) -> logging.Logger:
+        return self.__logger
+    
+    @property
+    def interface_manager(self) -> InterfaceManager:
+        return self.__interface_manager
+    
+    @property
+    def mac_table(self) -> MacTable:
+        return self.__mac_table
+    
+    @logger.setter
+    def logger(self, logger_lvl: str) -> None:
+        self.__logger.setLevel(logger_lvl)
+
+    def __setLogger(self, logger_lvl: str) -> logging.Logger:
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logger_lvl)
+        return logger
+    
+    def boot(self) -> None:
+        if self.__running:
+            raise SwitchIsActive
+        self.__running = True
+        self.logger.info('Switch is active')
