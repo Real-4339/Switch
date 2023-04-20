641f3c9e88f947ddd2ac1fca2b754cb0268418f2
Real-4339 <qvadym@stuba.sk>
2023-04-20 21:52:19 +0200

    added json funcs

--- switch/inter_mg.py
+++ switch/inter_mg.py
@@ -1,3 +1,6 @@
+import psutil
+import pyangbind.lib.pybindJSON as pybindJSON
+
 from typing import OrderedDict
 from templates.yang_py import sw_interface
 from exceptions import InterfaceDoesNotExist, InvalidState
@@ -6,6 +9,11 @@ from exceptions import InterfaceDoesNotExist, InvalidState
 class InterfaceManager:
     def __init__(self):
         self.__interface_container = sw_interface.interface().interfaces
+
+    def __boot(self) -> None:
+        for interface in psutil.net_if_addrs():
+            new_interface = self.__interface_container.interface_entry.add(interface)
+            new_interface.state = "down"
 
     def add_interface(self, interface_name: str) -> None:
         new_interface = self.__interface_container.interface_entry.add(interface_name)
@@ -39,3 +47,11 @@ class InterfaceManager:
         if interface_name not in self.__interface_container.interface_entry:
             raise InterfaceDoesNotExist
         self.__interface_container.interface_entry[interface_name].state = state
+
+    def return_json(self) -> OrderedDict:
+        return pybindJSON.dumps(self.__interface_container)
+    
+    def return_interface_json(self, interface_name: str) -> OrderedDict:
+        if interface_name not in self.__interface_container.interface_entry:
+            raise InterfaceDoesNotExist
+        return pybindJSON.dumps(self.__interface_container.interface_entry[interface_name])
