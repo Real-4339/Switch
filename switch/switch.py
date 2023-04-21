2b86902a40e507b7beb0c2226e07f56d4d7d5258
Real-4339 <qvadym@stuba.sk>
2023-04-21 16:11:41 +0200

    still doing

--- switch/switch.py
+++ switch/switch.py
@@ -1,29 +1,41 @@
 import logging
 
 from .mac import MacTable
+from .console import Console
 from .sniffer import Sniffer
 from scapy.packet import Packet
 from .inter_mg import InterfaceManager
-from .exceptions import SwitchIsActive, SwitchIsNotActive
+from .exceptions import SwitchIsActive, SwitchIsNotActive, InterfaceDoesNotExist
+
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+# Create a StreamHandler and set its level to INFO
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+
+# Create a formatter and add it to the handler
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# Add the handler to the logger
+log.addHandler(handler)
 
 
 class Switch:
     __version__ = '3.0.1'
-
+    
     def __init__(self) -> None:
         self.__running = False
         self.__mac_table = MacTable()
+        self.__console = Console(self)
         self.__interface_manager = InterfaceManager()
-        self.__working_interfaces = dict[str, Sniffer] = {}
-        self.logger = self.__setLogger('DEBUG')
+        self.__working_interfaces: dict[str, Sniffer] = {}
 
     @property
     def running(self) -> bool:
         return self.__running
-    
-    @property
-    def logger(self) -> logging.Logger:
-        return self.__logger
     
     @property
     def interface_manager(self) -> InterfaceManager:
@@ -33,32 +45,29 @@ class Switch:
     def mac_table(self) -> MacTable:
         return self.__mac_table
     
-    @logger.setter
-    def logger(self, logger_lvl: str) -> None:
-        self.__logger.setLevel(logger_lvl)
-
-    def __setLogger(self, logger_lvl: str) -> logging.Logger:
-        logger = logging.getLogger(__name__)
-        logger.setLevel(logger_lvl)
-        return logger
+    @property
+    def console(self) -> Console:
+        return self.__console
     
     def __packet_handler(self, interface: str, packet: Packet) -> None:
-        self.logger.info(f'Packet from {interface} is received')
+        log.info(f'Packet from {interface} is received')
 
-        self.__mac_table.add_entry(packet.src, self.__interface_manager.interfaces[interface])
+        self.__mac_table.add_or_update_entry(packet.src, interface)
         self.__mac_table.update()
 
     def boot(self) -> None:
         if self.__running:
             raise SwitchIsActive
-        self.__running = True
         self.__interface_manager.boot()
-        self.logger.info('Switch is active')
+        log.info('Switch is active')
 
     def choose_inter_to_run(self, interface_name: str) -> None:
         if self.__running:
             raise SwitchIsActive
+        if interface_name not in self.__interface_manager.get_interfaces():
+            raise InterfaceDoesNotExist
         self.__working_interfaces[interface_name] = Sniffer(interface_name, self.__packet_handler)
+        log.info(f'Interface {interface_name} is added to working interfaces')
 
     def delete_inter_to_run(self, interface_name: str) -> None:
         if self.__running:
@@ -66,22 +75,23 @@ class Switch:
         del self.__working_interfaces[interface_name]
 
     def run(self) -> None:
-        if not self.__running:
-            raise SwitchIsNotActive
+        if self.__running:
+            raise SwitchIsActive
+        self.__running = True
         for interface in self.__working_interfaces.values():
             interface.start()
-        self.logger.info('Switch is running')
+        log.info('Switch is running')
 
     def stop(self) -> None:
         if not self.__running:
             raise SwitchIsNotActive
         for interface in self.__working_interfaces.values():
             interface.stop()
-        self.logger.info('Switch is stopped')
+        self.__running = False
+        log.info('Switch is stopped')
 
     def shutdown(self) -> None:
-        if not self.__running:
-            raise SwitchIsNotActive
-        self.__running = False
+        if self.__running:
+            raise SwitchIsActive
         self.__interface_manager.shutdown()
-        self.logger.info('Switch is shutdown')
+        log.info('Switch is shutdown')
