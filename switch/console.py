d9d52eaafcc03a8d1e97e082b9a8ee24daf9ae46
Real-4339 <qvadym@stuba.sk>
2023-04-21 16:10:16 +0200

    creating async console

---
+++ switch/console.py
@@ -0,0 +1,56 @@
+import asyncio
+
+from aioconsole import ainput
+
+class Console:
+    def __init__(self, switch):
+        self.switch = switch
+        self._console = ainput
+        self.__running = False
+
+    def __execute(self, command):
+        if command == 'boot':
+            self.switch.boot()
+        elif command == 'run':
+            self.switch.run()
+        elif command == 'stop':
+            self.switch.stop()
+        elif command == 'shutdown':
+            self.switch.shutdown()
+        elif command == 'interfaces':
+            print(self.switch.interface_manager.return_json())
+        elif command.startswith('input interface:'):
+            inter_name = command.split(' ')[-1].strip()
+            self.switch.choose_inter_to_run(inter_name)
+        elif command.startswith('delete interface:'):
+            inter_name = command.split(' ')[-1].strip()
+            self.switch.delete_inter_to_run(inter_name)
+        elif command == 'show macs':
+            self.switch.mac_table.return_json()
+        elif command == 'exit':
+            try:
+                self.switch.stop()
+                self.switch.shutdown()
+            except:
+                pass
+            finally:
+                self.shutdown()
+        else:
+            print('Unknown command')
+
+    def boot(self):
+        self.__running = True
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.__start(loop))
+
+    async def __start(self, loop):
+        while self.__running:
+            command = await self._console('>>> ')
+            try:
+                await loop.run_in_executor(None, self.__execute, command)
+            except Exception as e:
+                print(e)
+
+    def shutdown(self):
+        print('Console is shutdown')
+        self.__running = False
